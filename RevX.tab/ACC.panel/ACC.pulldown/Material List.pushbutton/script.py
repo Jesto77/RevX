6768c8bd27d864fbd7692c0ad3e01c6bcb349adf
Jesto77 <jesto.joy@tangentlandscape.com>
2026-07-17 17:34:22 +0400

    updated acc link

--- RevX.tab/ACC.panel/ACC.pulldown/Material List.pushbutton/script.py
+++ RevX.tab/ACC.panel/ACC.pulldown/Material List.pushbutton/script.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 __title__ = "Material List"
-__author__ = "Your Name"
+__author__ = "Jesto Joy"
 
 import webbrowser
 
 def open_acc_link():
-    url = "https://acc.autodesk.com/docs/files/projects/12075e49-40e0-4d97-beee-941b933a1f20?folderUrn=urn%3Aadsk.wipprod%3Afs.folder%3Aco.iloPjGAGQvKnWqaqN3Weiw&entityId=urn%3Aadsk.wipprod%3Adm.lineage%3A7DJI8aKDQ8qxRF7qWRiNkw&viewModel=detail&moduleId=folders"
+    url = "https://acc.autodesk.com/docs/files/projects/12075e49-40e0-4d97-beee-941b933a1f20?folderUrn=urn%3Aadsk.wipprod%3Afs.folder%3Aco.iloPjGAGQvKnWqaqN3Weiw&entityId=urn%3Aadsk.wipprod%3Adm.lineage%3AZnYyDHBkTTa2VzWogoGaNA&viewModel=detail&moduleId=folders"
     webbrowser.open(url)
 
 if __name__ == "__main__":
